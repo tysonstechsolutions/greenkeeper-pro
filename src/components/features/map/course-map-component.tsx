12e921cbe76e914aa10c08de2c112bf0fbd720cf
tysonstechsolutions <tysonstechsolutions@gmail.com>
2026-03-27 23:11:26 -0500

    fix: switch map tiles to CartoDB CDN (OSM direct tiles returning 503)

--- src/components/features/map/course-map-component.tsx
+++ src/components/features/map/course-map-component.tsx
@@ -28,8 +28,8 @@ L.Icon.Default.mergeOptions({
 // Tile layer options with fallback
 const TILE_LAYERS = {
   osm: {
-    url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    url: "https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png",
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
     label: "Street Map",
   },
   satellite: {
